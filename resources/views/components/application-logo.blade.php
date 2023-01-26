0c336421a184d8f5a1f61ba675bcca23fd067992
Hussains3 <shussain8978@gmail.com>
2023-01-26 14:48:14 +0600

    logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
-<div class="flex items-center logo-div overflow-hidden">
+<div class="flex items-center logo-div overflow-hidden w-40">
     {{-- <object type="image/svg+xml" data="{{asset('dinghifdn.svg')}}" class="w-40"></object> --}}
     <img src="{{asset('dinghifdn.svg')}}" alt="" srcset="" class="w-40">
 </div>
